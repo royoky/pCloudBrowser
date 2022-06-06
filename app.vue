53cc4af4cceaedbe63380eed0d803abf4556c567
Ronan Marzin <ronan.marzin@protonmail.com>
2022-06-06 23:00:47 +0200

    initial commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <v-app>
+      <v-app-bar>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar-title>Vuetify App Bar Title</v-app-bar-title>
+      </v-app-bar>
+      <NuxtWelcome />
+    </v-app>
+  </div>
+</template>
